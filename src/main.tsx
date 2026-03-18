856cebfc2df78e357095394409c570a0d214834d
Diogo Ribas <diogo.meireles.ribas@gmail.com>
2026-03-18 21:57:07 +0000

    Moved Home into its own folder

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
-import Home from './pages/Home.tsx'
+import Home from './pages/Home/Home.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
