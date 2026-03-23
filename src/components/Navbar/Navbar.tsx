0a1a8200f5550136255bc1a1c5b8561702205a52
Diogo Ribas <diogo.meireles.ribas@gmail.com>
2026-03-23 22:29:18 +0000

    Standard Navbar css is finished.
    If time allows it this component might be updated to have a pseudo login feature that changes the user avatar and name

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -15,24 +15,24 @@
  * 
  */
 
-import { TableOfContents } from "lucide-react";
+import { CircleEllipsis } from "lucide-react";
 import styles from './Navbar.module.css';
 
 function Navbar() {
 
     return(
-        <nav>
-            <div>
+        <nav className={styles.navbar}>
+            <div className={styles['user-side']}>
                 <div className={styles['img-container']}>
                    <img src="https://cdn.jsdelivr.net/gh/faker-js/assets-person-portrait/female/512/96.jpg" alt="User Avatar" />
                 </div>
-                <div>
-                   <p>Welcome Back</p>
-                   <p>Gertudres Almeida</p>
+                <div className={styles['text-container']}>
+                   <p className={styles['welcome-text']}>Welcome Back</p>
+                   <p  className={styles.username}>Gertudes Almeida</p>
                 </div>
             </div>
-            <div>
-                <TableOfContents />
+            <div className={styles.icon}>
+                <CircleEllipsis size={30} />
             </div>
         </nav>
     );
