ef0481bc24a234b7c4c58c711bef46d57b9d62cb
Diogo Ribas <diogo.meireles.ribas@gmail.com>
2026-03-23 19:41:37 +0000

    Setup Guide for Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,19 @@
+/**
+ * NavBar
+ * 
+ * SearchBar
+ * 
+ * Carrousel
+ * 
+ * MyList
+ * 
+ * Recommended 
+ * 
+ * If Recommended or MyList are selected change into each respectively
+ * 
+ */
+
+
 function Home() {
     return <p>Hello World!</p>
 }
