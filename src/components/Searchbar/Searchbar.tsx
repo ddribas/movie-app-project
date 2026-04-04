15723ba1352c7cda363d26a15b94315ed6b5fd17
Diogo Ribas <diogo.meireles.ribas@gmail.com>
2026-04-04 14:32:10 +0100

    Finished the initial development of the searchbar

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,14 +1,17 @@
 import { Filter, Search } from "lucide-react";
+import styles from './Searchbar.module.css';
 
 function Searchbar () {
     return (
-        <section>
-            <div>
-                <Search />
-            </div>
-            <input />
-            <div>
-                <Filter />
+        <section className={styles.searchSection}>
+            <div className={styles.searchBar}>
+                <div>
+                    <Search />
+                </div>
+                <input type="text" placeholder="Search" />
+                <div>
+                    <Filter />
+                </div>
             </div>
         </section>
     );
